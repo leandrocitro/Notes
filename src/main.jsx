835f7a461148de3234843f791280ecd0068c7055
leandro.citrodonegit config --global user.name leandro.citro <leandro_citro@hotmail.com>
2024-03-02 23:24:52 -0300

    Definindo variáveis styled-components

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Details } from './pages/Details/index.jsx'
+import { ThemeProvider } from 'styled-components';
+import GlobalStyles from './styles/theme';
+
+import { Details } from './pages/Details'
+
+import theme from './styles/theme'
+
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Details />
-  </React.StrictMode>,
+    <ThemeProvider theme={theme}>
+      
+      <Details />
+    </ThemeProvider>
+  </React.StrictMode>
 )
