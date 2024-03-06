73cd355408c25d5fba9bba3b9d2f653134b7b477
leandro.citrodonegit config --global user.name leandro.citro <leandro_citro@hotmail.com>
2024-03-05 22:17:19 -0300

    Estilizando Menu

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -38,20 +38,29 @@ export const Brand = styled.div`
 
 export const Menu = styled.ul`
     grid-area: menu;
-    background: blue;
+    background-color: ${({ theme }) => theme.COLORS.BACKGROUND_900};
+
+    padding-top: 64px;
+    text-align: center;
+
+    > li {
+        margin-bottom: 24px;
+    }
+
+    
 `;
 
 export const Search = styled.div`
     grid-area: search;
-    background: green;
+    
 `;
 
 export const Content = styled.div`
     grid-area: content;
-    background: violet;
+    
 `;
 
 export const NewNote = styled.button`
     grid-area: newnote;
-    background: gray;
+   
 `;
